d58f72cac181a025aa511d2554978a567ca9adae
Maria Andreza C Santos <mandreza008@gmail.com>
2024-12-27 01:04:57 -0300

    adiciona funcao update

--- app.py
+++ app.py
@@ -39,6 +39,23 @@ def get_task(id):
             return jsonify(t.to_dict())
     return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
 
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks: 
+        if t.id == id:
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso."})
+
 # Rota de estudos
 @app.route('/user/<int:user_id>')
 def show_user(user_id):
